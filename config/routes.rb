4cef0e4a35d57a5b783eee42e15a878790d30ff8
Max Holzheu <maxhm10@gmail.com>
2014-04-23 23:45:28 -0600

    Create users and home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Quinielatest::Application.routes.draw do
+  resources :users
+
+  root 'static_pages#home'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
