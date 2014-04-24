96a39c4dd17c2b2845ccb5f99dcc106046a24dd5
Max Holzheu <maxhm10@gmail.com>
2014-04-23 19:05:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quinielatest::Application.config.secret_key_base = 'a53671ddb9eb869cacc8e3e0efd67e676601decf12cf00bb0899f9d184999ae9f99a233123164eaef5bc02b9bba6a890c105587743adf3230598a0ea523200d5'
